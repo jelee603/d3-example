b1d421ca7d727a39cac0f4f1212c77a4433f6b3e
jieun <jelee@ex-em.com>
2021-01-19 18:45:42 +0900

    add d3 rect

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('12345')
+import * as d3 from 'd3';
+
+const svg = d3.select('body')
+    .append('svg')
+    .attr('width', 500).attr('height', 200);
+svg.append('rect')
+    .attr('x', 10)
+    .attr('y', 10)
+    .attr('width', 80)
+    .attr('height', 80)
+    .attr('fill', 'blue');
